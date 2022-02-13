ba1204121acc575709502407a6dd7e678935e6ab
Jim Daehn <jdaehn@missouristate.edu>
2022-02-13 14:38:35 -0600

    Replace template unit tests.

--- test/tests.cpp
+++ test/tests.cpp
@@ -16,171 +16,788 @@
 
 #if TASK1_COMPLETED
 
-TEST(Task1, ConstantInitializedProperly)
+TEST(PointClassTestSuite, DefaultConstructorTest)
 {
-	EXPECT_EQ(csc232::NUM_ELEMENTS, 16);
+	// Given:
+	csc232::Point point{};
+
+	// When:
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+
+	// Then:
+	double expected_x{0};
+	double expected_y{0};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenItIsTheLastElement)
+TEST(PointClassTestSuite, OneArgConstructorTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 0, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x{1};
+	double y{0};
+	csc232::Point point{x};
+
+	// When:
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+
+	// Then:
+	double expected_x{x};
+	double expected_y{y};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenItIsTheFirstElement)
+TEST(PointClassTestSuite, TwoArgConstructorTest)
 {
-	csc232::IntArray data = {5, 4, 3, 2, 1};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 0, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x{1};
+	double y{1};
+	csc232::Point point{x, y};
+
+	// When:
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+
+	// Then:
+	double expected_x{x};
+	double expected_y{y};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenItIsTheMiddleElement)
+TEST(PointClassTestSuite, ShiftLeftTest)
 {
-	int data[5] = {3, 4, 5, 2, 1};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 0, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{-1};
+	double y_amount{0};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenItIsJustAfterFirstElement)
+TEST(PointClassTestSuite, ShiftRightTest)
 {
-	csc232::IntArray data = {1, 5, 2, 3, 4};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 0, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{1};
+	double y_amount{0};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenItIsJustBeforeLastElement)
+TEST(PointClassTestSuite, ShiftUpTest)
 {
-	csc232::IntArray data = {1, 2, 3, 5, 4};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 0, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{0};
+	double y_amount{1};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenLowerIndexGreaterThanZero)
+TEST(PointClassTestSuite, ShiftDownTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 3, 2, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{0};
+	double y_amount{-1};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenRangeGoesBeyondValidArrayIndexes)
+TEST(PointClassTestSuite, ShiftNorthEastTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, 3, 30, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{1};
+	double y_amount{1};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenLowerIndexIsLessThanZero)
+TEST(PointClassTestSuite, ShiftSoutEastTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, -3, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{1};
+	double y_amount{-1};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
-TEST(Task1, FindMaxWhenUpperAndLowerIndexesAreInvalid)
+TEST(PointClassTestSuite, ShiftNorthWestTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_max(data, -3, 50, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x_amount{1};
+	double y_amount{-1};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(PointClassTestSuite, ShiftSouthWestTest)
+{
+	// Given:
+	double x_amount{-1};
+	double y_amount{-1};
+	csc232::Point point{};
+
+	// When:
+	point.shift(x_amount, y_amount);
+
+	// Then:
+	double expected_x{x_amount};
+	double expected_y{y_amount};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(PointClassTestSuite, RotateFromQIToQII)
+{
+	// Given:
+	double x{1};
+	double y{2};
+	csc232::Point point{x, y};
+
+	// When
+	point.rotate90();
+
+	// Then
+	double expected_x{y};
+	double expected_y{-x};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(PointClassTestSuite, RotateFromQIIToQIII)
+{
+	// Given:
+	double x{1};
+	double y{-2};
+	csc232::Point point{x, y};
+
+	// When
+	point.rotate90();
+
+	// Then
+	double expected_x{y};
+	double expected_y{-x};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(PointClassTestSuite, RotateFromQIIIToQIV)
+{
+	// Given:
+	double x{-1};
+	double y{-2};
+	csc232::Point point{x, y};
+
+	// When
+	point.rotate90();
+
+	// Then
+	double expected_x{y};
+	double expected_y{-x};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(PointClassTestSuite, RotateFromQIVToQI)
+{
+	// Given:
+	double x{-1};
+	double y{2};
+	csc232::Point point{x, y};
+
+	// When
+	point.rotate90();
+
+	// Then
+	double expected_x{y};
+	double expected_y{-x};
+	double actual_x{point.get_x()};
+	double actual_y{point.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
 }
 
 #endif // TASK1_COMPLETED
 
 #if TASK2_COMPLETED
 
-TEST(Task2, FindSumOfAllElements)
+TEST(RotationsNeededTestSuite, NoRotationNeededTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{15};
-	const int ACTUAL = csc232::find_sum(data, 0, 5, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x{1};
+	double y{1};
+	csc232::Point point{x, y};
+
+	// When:
+	int actual{csc232::rotations_needed(point)};
+
+	// Then:
+	int expected{0};
+	EXPECT_EQ(expected, actual);
 }
 
-TEST(Task2, FindSumOfFirstTwoElements)
+TEST(RotationsNeededTestSuite, OneRotationNeededTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{3};
-	const int ACTUAL = csc232::find_sum(data, 0, 2, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x{-1};
+	double y{1};
+	csc232::Point point{x, y};
+
+	// When:
+	int actual{csc232::rotations_needed(point)};
+
+	// Then:
+	int expected{1};
+	EXPECT_EQ(expected, actual);
 }
 
-TEST(Task2, FindSumOfLastTwoElements)
+TEST(RotationsNeededTestSuite, TwoRotationsNeededTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{9};
-	const int ACTUAL = csc232::find_sum(data, 3, 2, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Given:
+	double x{-1};
+	double y{-1};
+	csc232::Point point{x, y};
+
+	// When:
+	int actual{csc232::rotations_needed(point)};
+
+	// Then:
+	int expected{2};
+	EXPECT_EQ(expected, actual);
 }
 
-TEST(Task2, FindSumOfFirstElement)
+TEST(RotationsNeededTestSuite, ThreeRotationsNeededTest)
 {
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{1};
-	const int ACTUAL = csc232::find_sum(data, 0, 1, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
-}
+	// Given:
+	double x{1};
+	double y{-1};
+	csc232::Point point{x, y};
 
-TEST(Task2, FindSumOfLastElement)
-{
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{5};
-	const int ACTUAL = csc232::find_sum(data, 4, 1, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
-}
+	// When:
+	int actual{csc232::rotations_needed(point)};
 
-TEST(Task2, FindSumOfMiddleElement)
-{
-	csc232::IntArray data = {1, 2, 3, 4, 5};
-	const int EXPECTED{3};
-	const int ACTUAL = csc232::find_sum(data, 2, 1, 5);
-	EXPECT_EQ(EXPECTED, ACTUAL);
+	// Then:
+	int expected{3};
+	EXPECT_EQ(expected, actual);
 }
 
 #endif // TASK2_COMPLETED
 
 #if TASK3_COMPLETED
 
-class Task3 : public ::testing::Test {
-protected:
-	void SetUp() override {
-		testing::internal::CaptureStdout();
-	}
-
-	void TearDown() override {
-		if (!HasFailure())
-		{
-		}
-	}
-
-	// Reusable objects for each unit test in this test fixture
-	csc232::TwoDimArray table{{1, 2, 3},
-						{4, 5, 6},
-						{7, 8, 9}};
-};
-
-TEST_F(Task3, PrintFullTableTestDefaultFieldWidth)
+TEST(RotationToFirstQuadrantTestSuite, RotateFromFirstQuadrant)
 {
-	csc232::print_table(table, 3);
-	std::string expected{"    1    2    3\n    4    5    6\n    7    8    9\n"};
-	std::string actual{testing::internal::GetCapturedStdout()};
-	EXPECT_EQ(expected, actual);
+	// Given:
+	double x{1};
+	double y{1};
+	csc232::Point point{x, y};
+
+	// When:
+	csc232::rotate_to_upper_right(point);
+
+	// Then:
+	EXPECT_TRUE(point.get_x() >= 0);
+	EXPECT_TRUE(point.get_y() >= 0);
 }
 
-TEST_F(Task3, PrintFullTableTestCustomFieldWidth)
+TEST(RotationToFirstQuadrantTestSuite, RotateFromSecondQuadrant)
 {
-	csc232::print_table(table, 3, 7);
-	std::string expected{"      1      2      3\n      4      5      6\n      7      8      9\n"};
-	std::string actual{testing::internal::GetCapturedStdout()};
-	EXPECT_EQ(expected, actual);
+	// Given:
+	double x{1};
+	double y{-1};
+	csc232::Point point{x, y};
+
+	// When:
+	csc232::rotate_to_upper_right(point);
+
+	// Then:
+	EXPECT_TRUE(point.get_x() >= 0);
+	EXPECT_TRUE(point.get_y() >= 0);
+}
+
+TEST(RotationToFirstQuadrantTestSuite, RotateFromThirdQuadrant)
+{
+	// Given:
+	double x{-1};
+	double y{-1};
+	csc232::Point point{x, y};
+
+	// When:
+	csc232::rotate_to_upper_right(point);
+
+	// Then:
+	EXPECT_TRUE(point.get_x() >= 0);
+	EXPECT_TRUE(point.get_y() >= 0);
+}
+
+TEST(RotationToFirstQuadrantTestSuite, RotateFromFourthQuadrant)
+{
+	// Given:
+	double x{-1};
+	double y{1};
+	csc232::Point point{x, y};
+
+	// When:
+	csc232::rotate_to_upper_right(point);
+
+	// Then:
+	EXPECT_TRUE(point.get_x() >= 0);
+	EXPECT_TRUE(point.get_y() >= 0);
 }
 
 #endif // TASK3_COMPLETED
+
+#if TASK4_COMPLETED
+
+TEST(DistanceTestSuite, DistanceFromOriginToFirstQuadrantPointTest)
+{
+	// Given:
+	double x1{0};
+	double y1{0};
+	double x2{3};
+	double y2{4};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, DistanceFromOriginToSecondQuadrantPointTest)
+{
+	// Given:
+	double x1{0};
+	double y1{0};
+	double x2{3};
+	double y2{-4};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, DistanceFromOriginToThirdQuadrantPointTest)
+{
+	// Given:
+	double x1{0};
+	double y1{0};
+	double x2{-3};
+	double y2{-4};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, DistanceFromOriginToFourthQuadrantPointTest)
+{
+	// Given:
+	double x1{0};
+	double y1{0};
+	double x2{-3};
+	double y2{4};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, RightHorizontalDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{2};
+	double y2{1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{1};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, LefttHorizontalDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{-2};
+	double y2{1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{3};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, PositiveVerticalDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{1};
+	double y2{2};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{1};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, NegativeVerticalDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{1};
+	double y2{-2};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{3};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, NorthEastDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{4};
+	double y2{5};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, SouthEastDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{4};
+	double y2{-3};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, SouthWestDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{-2};
+	double y2{-3};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+TEST(DistanceTestSuite, NorthWestDistanceTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{-2};
+	double y2{5};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	double actual_distance{csc232::distance(p1, p2)};
+
+	// Then:
+	double expected_distance{5};
+	EXPECT_DOUBLE_EQ(expected_distance, actual_distance);
+}
+
+#endif // TASK4_COMPLETED
+
+#if TASK5_COMPLETED
+
+TEST(MidpointTestSuite, MidpointOfHorizontalLineTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{-1};
+	double y2{1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{0};
+	double expected_y{1};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(MidpointTestSuite, MidpointOfVerticalLineTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{1};
+	double y2{-1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{1};
+	double expected_y{0};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(MidpointTestSuite, MidpointOfNorthEastLineTest)
+{
+	// Given:
+	double x1{-1};
+	double y1{-1};
+	double x2{1};
+	double y2{1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{0};
+	double expected_y{0};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(MidpointTestSuite, MidpointOfSouthEastLineTest)
+{
+	// Given:
+	double x1{-1};
+	double y1{1};
+	double x2{1};
+	double y2{-1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{0};
+	double expected_y{0};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(MidpointTestSuite, MidpointOfSouthWestLineTest)
+{
+	// Given:
+	double x1{1};
+	double y1{1};
+	double x2{-1};
+	double y2{-1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{0};
+	double expected_y{0};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(MidpointTestSuite, MidpointOfNorthWestLineTest)
+{
+	// Given:
+	double x1{1};
+	double y1{-1};
+	double x2{-1};
+	double y2{1};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x2, y2};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{0};
+	double expected_y{0};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+TEST(MidpointTestSuite, MidpointOfTheSamePoints)
+{
+	// Given:
+	double x1{-3};
+	double y1{19};
+	csc232::Point p1{x1, y1};
+	csc232::Point p2{x1, y1};
+
+	// When:
+	csc232::Point middle{csc232::middle(p1, p2)};
+
+	// Then:
+	double expected_x{x1};
+	double expected_y{y1};
+	double actual_x{middle.get_x()};
+	double actual_y{middle.get_y()};
+	EXPECT_DOUBLE_EQ(expected_x, actual_x);
+	EXPECT_DOUBLE_EQ(expected_y, actual_y);
+}
+
+#endif // TASK5_COMPLETED
